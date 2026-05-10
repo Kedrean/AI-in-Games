b5065f99c88876aecb0e0047e3300e85930d2bf7
Kedrean <iubtapales@gmail.com>
2026-05-10 10:51:42 +0800

    AI patrol paths

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Patrol : MonoBehaviour
 {
+    public Transform[] patrolPoints;
+
+    private NavMeshAgent agent;
+    private int currentPointIndex = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        if (patrolPoints.Length > 0)
+        {
+            agent.SetDestination(patrolPoints[currentPointIndex].position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            GoToNextPoint();
+        }
+    }
+
+    void GoToNextPoint()
+    {
+        currentPointIndex++;
+
+        if (currentPointIndex >= patrolPoints.Length)
+        {
+            currentPointIndex = 0;
+        }
+
+        agent.SetDestination(patrolPoints[currentPointIndex].position);
     }
 }
