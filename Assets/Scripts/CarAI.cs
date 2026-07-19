55226aa8df17da1cfc0c1efa1d68c3ec2f89e891
Kedrean <iubtapales@gmail.com>
2026-07-19 23:02:49 +0800

    updated script

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -3,19 +3,26 @@ using UnityEngine.AI;
 
 public class CarAI : MonoBehaviour
 {
+    [Header("Waypoints")]
     public Transform[] destination;
     public int currentWaypointIndex = 0;
 
+    [Header("Traffic Light")]
     public StopLight trafficLight;
     public Transform stopPoint;
     public float stopDistance = 2f;
 
-    public float normalSpeed;
-    public float slowSpeed;
+    [Header("Car Speeds")]
+    public float normalSpeed = 5f;
+    public float slowSpeed = 2.5f;
+
+    [Header("Car Detection")]
+    public float carDetectionDistance = 4f;
+    public float carDetectionRadius = 0.6f;
+    public LayerMask carLayer;
 
     private NavMeshAgent carAgent;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         carAgent = GetComponent<NavMeshAgent>();
@@ -24,15 +31,16 @@ public class CarAI : MonoBehaviour
         MoveToCurrentWaypoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
         FollowTrafficLight();
 
-        if (carAgent.pathPending)
-        {
+        // Don't continue if stopped
+        if (carAgent.isStopped)
             return;
-        }
+
+        if (carAgent.pathPending)
+            return;
 
         if (carAgent.remainingDistance <= carAgent.stoppingDistance)
         {
@@ -42,20 +50,29 @@ public class CarAI : MonoBehaviour
 
     private void FollowTrafficLight()
     {
+        // Stop if another car is directly ahead
+        if (IsCarAhead())
+        {
+            carAgent.isStopped = true;
+            carAgent.speed = 0;
+            return;
+        }
+
         float distanceToStopPoint = Vector3.Distance(transform.position, stopPoint.position);
 
+        // Red light - stop
         if (!trafficLight.isGreen && distanceToStopPoint <= stopDistance * 2)
         {
             carAgent.isStopped = true;
             carAgent.speed = 0;
         }
-
+        // Red light - slow down
         else if (!trafficLight.isGreen && distanceToStopPoint <= stopDistance * 4)
         {
             carAgent.isStopped = false;
             carAgent.speed = slowSpeed;
         }
-
+        // Green light - normal speed
         else
         {
             carAgent.isStopped = false;
@@ -63,8 +80,33 @@ public class CarAI : MonoBehaviour
         }
     }
 
+    private bool IsCarAhead()
+    {
+        Vector3 origin = transform.position + Vector3.up * 0.5f;
+
+        if (Physics.SphereCast(
+                origin,
+                carDetectionRadius,
+                transform.forward,
+                out RaycastHit hit,
+                carDetectionDistance,
+                carLayer))
+        {
+            // Ignore ourselves
+            if (hit.transform != transform)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private void MoveToCurrentWaypoint()
     {
+        if (destination.Length == 0)
+            return;
+
         carAgent.SetDestination(destination[currentWaypointIndex].position);
     }
 
@@ -78,5 +120,15 @@ public class CarAI : MonoBehaviour
         }
 
         MoveToCurrentWaypoint();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Vector3 origin = transform.position + Vector3.up * 0.5f;
+
+        Gizmos.DrawLine(origin, origin + transform.forward * carDetectionDistance);
+        Gizmos.DrawWireSphere(origin + transform.forward * carDetectionDistance, carDetectionRadius);
     }
 }
